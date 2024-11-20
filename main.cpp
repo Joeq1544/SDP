4c9f2b97fec663e9e59f281d2772186334ad13a8
Joeq1544 <139137968+Joeq1544@users.noreply.github.com>
2024-11-20 11:29:17 -0500

    base menu complete

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <FEHRandom.h>
 
 void drawMenu();
-void navigateMenu();
+//void navigateMenu();
 void displayGame();
 void displayStats();
 void displayInstructions();
@@ -15,8 +15,8 @@ int main() {
     while (running)
     {
         LCD.Update();
+        FEHIcon::Icon *buttons;
         drawMenu();
-        navigateMenu();
         running = false;
     }
     while(1)
@@ -28,23 +28,23 @@ int main() {
 
 void drawMenu()
 {
+    using namespace FEHIcon;
     LCD.SetBackgroundColor(BLACK);
-    LCD.SetFontColor(BLUE);
+    LCD.SetFontColor(RED);
     LCD.Clear();
-    LCD.FillRectangle(50,50,25,25);
-    LCD.FillRectangle(50,100,25,25);
-    LCD.FillRectangle(100,50,25,25);
-    LCD.FillRectangle(100,100,25,25);
-    LCD.WriteAt("Play",55,55);
-    LCD.WriteAt("Stats",55,105);
-    LCD.WriteAt("Instructions",105,55);
-    LCD.WriteAt("Credits",105,105);
+    Icon gameButton;
+    Icon statButton;
+    Icon instructionButton;
+    Icon creditButton;
+    gameButton.SetProperties("Play", 40, 26, 240, 26, WHITE, RED);
+    statButton.SetProperties("Stats", 40, 78, 240, 26, WHITE, RED);
+    instructionButton.SetProperties("Instructions", 40, 130, 240, 26, WHITE, RED);
+    creditButton.SetProperties("Credits", 40, 182, 240, 26, WHITE, RED);
+    gameButton.Draw();
+    statButton.Draw();
+    instructionButton.Draw();
+    creditButton.Draw();
     LCD.Update();
-}
-
-void navigateMenu()
-{
-    //LCD.ClearBuffer();
     int choice = -1;
     int x;
     int y;
@@ -54,10 +54,25 @@ void navigateMenu()
     {
         while(!LCD.Touch(&x,&y)){};
         while(LCD.Touch(&xtrash,&ytrash)){};
-        if(x >= 50 && x <= 75 && y >= 50 && y <= 75)
+        if(gameButton.Pressed(x,y,1)==1)
         {
-            choice = 1;
             displayGame();
+            choice = 1;
+        }
+        if(statButton.Pressed(x,y,1)==1)
+        {
+            displayStats();
+            choice = 1;
+        }
+        if(instructionButton.Pressed(x,y,1)==1)
+        {
+            displayInstructions();
+            choice = 1;
+        }
+        if(creditButton.Pressed(x,y,1)==1)
+        {
+            displayCredits();
+            choice = 1;
         }
     }
 }
@@ -81,7 +96,10 @@ void displayStats()
 void displayInstructions()
 {
     LCD.Clear();
-    LCD.WriteLine("Instructions here.");
+    LCD.WriteLine("Set the angle of the launch.");
+    LCD.WriteLine("Hit the launch button when ready.");
+    LCD.WriteLine("Collect points from distance traveled.");
+    LCD.WriteLine("Buy upgrades with your points and keep playing!");
     LCD.Update();
 }
 
