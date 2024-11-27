9b8e867d57d9d1983d134be1e266c15c1ee75a9e
taylor4535 <taylor.4535@buckeyemail.osu.edu>
2024-11-27 09:34:19 -0500

    Added some comments, formatting fixes
    small formatting fixes, added comments to code I understood. please add your own comments to the code though; it's required for points and a lot of this stuff we definitely did not cover in class, so it would just clear up what's going on anyway

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ using namespace FEHIcon;
 using namespace std;
 
 //CONSTANTS
-#define deltaFrameTime 1/5.0
-#define playerMass 10
-#define obMass 1
+#define deltaFrameTime 1/5.0 // fps of the game
+#define playerMass 10 // initial player mass for collision physics
+#define obMass 1 // initial obstacle mass for collision physics
 
 //
 //FUNCTIONS DECLARATIONS NOT ATTACHED TO A CLASS
@@ -84,27 +84,27 @@ class gameState
 {
     private:
         Player player;
-        float startingVelocity;
+        float startingVelocity; // initial speed of the launch
         int launches; // total number of times played during code running
-        float arcLength = 0;
-        float pointsTot = 0;
-        float previousDistance = 0;
-        float maxDistance = 0;
+        float arcLength = 0; // arc length of the current round
+        float pointsTot = 0; // current number of total points
+        float previousDistance = 0; // arc length of the previous round - to display during stats screen
+        float maxDistance = 0; // max distance traveled - for stats screen
         float maxHeight = 0; // arc length for each run and total points collected
     public:
-        gameState(int launches, float arcLength, float pointsTot, float startingVelocity, float previousDistance, float maxDistance, float maxHeight);
+        gameState(int launches, float arcLength, float pointsTot, float startingVelocity, float previousDistance, float maxDistance, float maxHeight); // constructor function
         void initPlayer(float xPos, float yPos, float xVel, float yVel, float screenXPos, float screenYPos, float mass);
-        void drawMenu();
-        void displayGame();
-        void runGame();
-        void displayStats();
-        void displayInstructions();
-        void displayCredits();
-        void returnToMainMenu();
-        void SetUpgrades(); // code mostly completed
-        void SetAngle(); // buttons dealt with, left x/y velocity calculations up to you like you said
-        void UpdatePosition(); // code completed
-        void EndScreen(); // code completed
+        void drawMenu(); // displays the main menu to the screen
+        void displayGame(); // runs the actual game itself
+        void runGame(); // actual launching projectile simulation
+        void displayStats(); // displays the statistics page to the screen
+        void displayInstructions(); // displays the instructions page to the screen
+        void displayCredits(); // displays the credits page to the screen
+        void returnToMainMenu(); // button for returning to the main menu - made to simplify code due to repetition
+        void SetUpgrades(); // allow for extra upgrades
+        void SetAngle(); // set initial angle for the current run
+        void UpdatePosition(); // update position on screen
+        void EndScreen(); // final results screen, prompts user to play again/return to menu
 };
 
 //
@@ -133,7 +133,7 @@ int main() {
 //FUNCTION DEFINITIONS NOT ATTACHED TO A CLASS
 //
 
-void clearScreen()
+void clearScreen() // reset screen for frames, draw bordering outline
 {
     LCD.Clear();
     LCD.DrawRectangle(0,0,319,239);
@@ -184,7 +184,7 @@ void gameState :: drawMenu()
     LCD.Update();
     // define extra variables for the while loop
     int choice = -1; // checks if the user has clicked a button
-    int x, y;
+    int x, y; // location of user's click
     int xtrash, ytrash; // throwaway values
     // check if the user has clicked, then check which button - run respective menu/game
     while(choice < 0)
@@ -216,11 +216,11 @@ void gameState :: drawMenu()
 
 void gameState :: displayGame() // runs the game itself
 {
-    initPlayer(0,0,0,0,20,220, playerMass);
-    SetUpgrades();
-    SetAngle();
-    runGame();
-    EndScreen();
+    initPlayer(0,0,0,0,20,220, playerMass); // start by intitializing projectile's values
+    SetUpgrades(); // prompt user to enter upgrades/skip
+    SetAngle(); // prompt user to set initial angle
+    runGame(); // run projectile sim w/ provided values
+    EndScreen(); // display final results, prompt to return to menu/play again
 }
 
 void gameState :: runGame()
@@ -283,7 +283,7 @@ void gameState :: runGame()
 void gameState :: displayStats() // statistics menu
 {
     LCD.Clear();
-    // display stats - total distance of arc length, maximum arc length, and total launches played
+    // display stats - total distance of arc length, maximum arc length, total launches played, maximum distance/height
     LCD.WriteLine("Launches: ");
     LCD.WriteLine(launches);
     LCD.WriteLine("Previous Distance(m): ");
@@ -292,7 +292,7 @@ void gameState :: displayStats() // statistics menu
     LCD.WriteLine(maxDistance);
     LCD.WriteLine("Max Height(m): ");
     LCD.WriteLine(maxHeight);
-    LCD.WriteLine("Total Distance(m): ");
+    LCD.WriteLine("Current Points Saved: ");
     LCD.WriteLine(pointsTot);
     // prompt user to return to main menu
     returnToMainMenu();
@@ -333,7 +333,7 @@ void gameState :: returnToMainMenu()
     // check if/when menu button is clicked
     // define extra variables for the while loop
     int choice = -1; // checks if the user has clicked a button
-    int x, y;
+    int x, y; // location of user's click
     int xtrash, ytrash; // throwaway values
     // check if the user has clicked, then check if menu button was clicked
     while(choice < 0)
@@ -359,15 +359,15 @@ void gameState::SetUpgrades() // prompt the user to either purchase a power-up o
 {
     // display points/options to screen
     LCD.Clear();
-    LCD.Write("Total points:");
+    LCD.WriteLine("Total points:");
     LCD.WriteLine(pointsTot);
     LCD.WriteLine("Choose a powerup:");
 
     // make buttons for each powerup available, display to screen
     Icon speedButton, gliderButton, skipButton;
-    speedButton.SetProperties("2x speed-5,000", 20, 78, 280, 26, WHITE, RED);
+    speedButton.SetProperties("x2 speed-5,000", 20, 78, 280, 26, WHITE, RED);
     speedButton.Draw();
-    gliderButton.SetProperties("x0.75gravity-10,000", 20, 130, 280, 26, WHITE, RED);
+    gliderButton.SetProperties("x0.75 gravity-10,000", 20, 130, 280, 26, WHITE, RED);
     gliderButton.Draw();
     skipButton.SetProperties("Skip Powerups", 20, 182, 280, 26, WHITE, RED);
     skipButton.Draw();
@@ -377,7 +377,7 @@ void gameState::SetUpgrades() // prompt the user to either purchase a power-up o
     // check if/when one of the buttons are clicked
     // define extra variables for the while loop
     int choice = -1; // checks if the user has clicked a button
-    int x, y;
+    int x, y; // location of user's click
     int xtrash, ytrash; // throwaway values
     // check if the user has clicked, then check which button - run respective function
     while(choice < 0)
@@ -390,8 +390,8 @@ void gameState::SetUpgrades() // prompt the user to either purchase a power-up o
             // check if user has enough points - if yes, run function/reduce points otherwise do nothing
             if (pointsTot >=5000)
             {
-                pointsTot-=5000;
-                startingVelocity *= 2;
+                pointsTot-=5000; // decrease points by given cost
+                startingVelocity *= 2; // doubles the starting velocity
                 choice = 1;
             }
         }
@@ -400,8 +400,8 @@ void gameState::SetUpgrades() // prompt the user to either purchase a power-up o
             // decrease gravity by 25% - can increase several times?
             if (pointsTot >= 10000)
             {
-                pointsTot-=10000;
-                player.multiplyAccel(.75);
+                pointsTot-=10000; // decrease by given cost value
+                player.multiplyAccel(.75); // 3/4 the projectile's acceleration due to gravity
                 choice = 1;
             }
         }
@@ -418,11 +418,11 @@ void gameState::SetAngle() // prompt the user to set the launch angle for the pr
 {
     LCD.Clear();
     // prompt user to set the initial angle
-    LCD.WriteLine("Choose an initial launch angle:");
+    LCD.WriteLine("Choose a launch angle:");
 
     // create buttons for each of the angles used - 30/45/60/90
-    Icon thirtyButton, fortyfiveButton, sixtyButton, ninetyButton;
-    thirtyButton.SetProperties("30 degrees", 40, 26, 240, 26, WHITE, RED);
+    Icon thirtyButton, fortyfiveButton, sixtyButton, ninetyButton; // buttons for each of the angles
+    thirtyButton.SetProperties("30 degrees", 40, 26, 240, 26, WHITE, RED); // setting up buttons' values
     fortyfiveButton.SetProperties("45 degrees", 40, 78, 240, 26, WHITE, RED);
     sixtyButton.SetProperties("60 degrees", 40, 130, 240, 26, WHITE, RED);
     ninetyButton.SetProperties("90 degrees", 40, 182, 240, 26, WHITE, RED);
@@ -435,7 +435,7 @@ void gameState::SetAngle() // prompt the user to set the launch angle for the pr
     LCD.Update();
     // define extra variables for the while loop
     int choice = -1; // checks if the user has clicked a button
-    int x, y;
+    int x, y; // location of click
     int xtrash, ytrash; // throwaway values
     // check if the user has clicked, then check if the user clicked an angle button - fix x/y velocities by trigonometry
     while(choice < 0)
@@ -444,7 +444,7 @@ void gameState::SetAngle() // prompt the user to set the launch angle for the pr
         while(LCD.Touch(&xtrash,&ytrash)){};
         if(thirtyButton.Pressed(x,y,1)==1)
         {
-            // vx = v*cos(30), vy = v*sin(30) - just remember we can't use functions, so check what the values are
+            // vx = v*cos(30), vy = v*sin(30) 
             player.init(0,0,-startingVelocity*cos(60),startingVelocity*sin(60),20,220, playerMass);
             choice = 1;
         }
@@ -480,20 +480,20 @@ void gameState::EndScreen() // displays the final results of the game, accepting
 // don't need pointers since none of the values need to be modified
 {
     // increase total launches for stats screen, increase total points by given arclength
-    launches++;
-    pointsTot+=arcLength;
+    launches++; // increase total launches for stat screen
+    pointsTot+=arcLength; // add arc length to current point value
     cout << arcLength <<" al "<< pointsTot<<" pt\n";
-    previousDistance = arcLength;
+    previousDistance = arcLength; // fix issue in stat screen - set previous AL to the AL that just ran
     LCD.Clear();
     // tell the user how many points they've earned/how far they went and their current point total
-    LCD.Write("Distance Traveled: ");
+    LCD.WriteLine("Distance Traveled: ");
     LCD.WriteLine(arcLength);
     LCD.Write("Total Points: ");
     LCD.WriteLine(pointsTot);
     arcLength = 0;
 
     // prompt the user to either play again or return to menu
-    Icon menuButton, resetButton;
+    Icon menuButton, resetButton; // buttons for the main menu/playing again
     menuButton.SetProperties("Return to Menu", 40, 182, 240, 26, WHITE, RED);
     menuButton.Draw();
     resetButton.SetProperties("Play Again", 40, 130, 240, 26, WHITE, RED);
@@ -504,7 +504,7 @@ void gameState::EndScreen() // displays the final results of the game, accepting
     // check if/when one of the buttons are clicked
     // define extra variables for the while loop
     int choice = -1; // checks if the user has clicked a button
-    int x, y;
+    int x, y; // location of click
     int xtrash, ytrash; // throwaway values
     // check if the user has clicked, then check which button - run respective function
     while(choice < 0)
